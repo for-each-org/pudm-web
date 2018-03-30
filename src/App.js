da300999f68119117aa5e7157afec1cdcf1e88f1
vritant24 <vritoku@gmail.com>
2018-03-29 20:15:05 -0400

    added stuff to help test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import components from './components';
 
 class App extends Component {
   render() {
+    // <components.form.Button/> you can put a component like this between the divs below to display and test it.
+    // I suggest using console.log to test onSelect and onClick stuff
     return (
       <div>
+        
       </div>
     );
   }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,21 @@
-import ListElement from './notification-display/ListElement';
+import ListElement      from './notification-display/ListElement';
+import Button           from './form/Button';
+import GroupSelector    from './form/GroupSelector';
+import TextField        from './form/Textfield';
+import TimePicker       from './form/TimePicker';
 
 const notifications = {
-    ListElement,
-}
+    ListElement
+};
+
+const form = {
+    Button,
+    GroupSelector,
+    TextField,
+    TimePicker
+};
+
+export default {
+    notifications,
+    form,
+};
