e3917344388bce9045e65e5d5bb2461f4f64add0
vritant24 <vritoku@gmail.com>
2018-03-31 14:03:53 -0400

    added NotificationList and NotificationEditor templates

---
+++ src/containers/NotificationEditor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class NotificationEditor extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <div/>
+        )
+    }
+}
