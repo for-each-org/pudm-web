cc83e5cfab12f79812cd5e31ac6d1ac671ea7bcf
vritant24 <vritoku@gmail.com>
2018-03-31 13:58:29 -0400

    created functions in dashboard, refactored file structture for containers

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -9,11 +9,45 @@ export default Dashboard extends Component {
             form: {
                 textfield: null,
                 time: null,
-                group: null
+                group: null,
+                edit: false
             }
         }
     }
     
+    onClickNotificationListItem(notif_id) {
+        //Populate form with notification details
+        this.setState(prev => (
+            const notif = this.state.notifications.notif_id;
+            {
+            form: {
+                textfield: notif.content,
+                time: notif.time,
+                group: notif.group,
+                edit: true
+            }
+        });
+    }
+
+    onSubmitForm(form) {
+        //TODO : Communicate with server
+        
+        //TODO : display confirmation of submission of form
+
+        //Reset Form
+        this.setState(prev => (
+            const notif = this.state.notifications.notif_id;
+            {
+            form: {
+                textfield: null,
+                time: null,
+                group: null,
+                edit: false
+            }
+        });
+
+    }
+
     render() {
         return(
             <div/>
