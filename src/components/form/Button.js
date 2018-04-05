7d46c76a580df81486183645760d457475268c15
QC <cheng305@purdue.edu>
2018-04-05 19:55:36 -0400

    Button completed

--- src/components/form/Button.js
+++ src/components/form/Button.js
@@ -9,6 +9,8 @@ import React from 'react';
  */
 
 export default function Button(props) {
+
+    
     return (
         <div>
           <button onClick={props.onClick}>{props.display_name}</button>
