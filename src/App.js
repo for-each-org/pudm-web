4dc6fbe895c7b771b1063e7411348af7f49fca8f
QC <cheng305@purdue.edu>
2018-04-07 14:15:13 -0400

    Add input for TimePicker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import components from './components';
 
 class App extends Component {
+
   render() {
     // <components.form.Button/> you can put a component like this between the divs below to display and test it.
     // I suggest using console.log to test onSelect and onClick stuff
