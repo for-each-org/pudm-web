f3235fccd66990688ad7e602b5c3ab34a01a63b9
Nolan Sharpless <nsharple@purdue.edu>
2018-04-12 20:12:31 -0400

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import components           from './components';
-import containers           from './containers';
+import components from './components';
+import container from './containers';
 
 const groups = [
-    "red", "blue", "green"
+    "red", "blue", "green", "yellow", "orange"
 ];
 
 var content = "";
 
 
 class App extends Component {
-<<<<<<< HEAD
     render() {
         // <components.form.Button/> you can put a component like this between the divs below to display and test it.
         // I suggest using console.log to test onSelect and onClick stuff
         return ( 
             <div>
-                <components.form.TextField/>
-                <components.form.GroupSelector group_list={groups}/>
+                <container.NotificationEditor/>
             </div>
         );
     }
