f043b12a81727e54251520b44c88876cccf35c14
vritant24 <vritoku@gmail.com>
2018-04-19 18:30:34 -0400

    added db communication to dashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import components from './components';
-import container from './containers';
-import db from './db/db_comm';
+import Dashboard from './containers';
 
 class App extends Component {
     render() {
         // <components.form.Button/> you can put a component like this between the divs below to display and test it.
         // I suggest using console.log to test onSelect and onClick stuff
         return ( 
-            <div>
-                <container.NotificationEditor/>
-            </div>
+            <Dashboard/>
         );
     }
 }
