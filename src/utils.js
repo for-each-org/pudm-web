fed5edb0d47cb7204af0163bf6bd524ec932c846
vritant24 <vritoku@gmail.com>
2018-04-28 14:41:22 -0400

    added function for parsing time and date to unix time

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,8 @@ function getTimeString(unix_time) {
     const hours = ("0" + date.getHours()).slice(-2);
     const minutes = ("0" + date.getMinutes()).slice(-2);
     const seconds = ("0" + date.getSeconds()).slice(-2);
-    return hours + ":" + minutes + ":" + seconds;
+
+    return hours + ":" + minutes + ":" + seconds; //because timepicker likes it like this
 }
 
 function getDateString(unix_time) {
@@ -15,10 +16,24 @@ function getDateString(unix_time) {
     const day   = ("0" + date.getDate()).slice(-2);
     const month = ("0" + (date.getMonth() + 1)).slice(-2);
     const year  = date.getFullYear();
-    return year + "-" + month + "-" + day;
+
+    return year + "-" + month + "-" + day; //because datepicker likes it like this
+}
+
+function dateTimeToUnix(date, time) {
+    //date: "yyyy-mm-dd"
+    //time: "hh:mm:ss"
+
+    const date_mod = date.replace("-", "/").replace("-","/"); //change to yyyy/mm/dd format
+    const complete_date = date_mod + " " + time;
+    
+    const unix_time = (new Date(complete_date)).getTime() / 1000;
+    
+    return Math.round(unix_time);
 }
 
 export default {
     getDateString,
-    getTimeString
+    getTimeString,
+    dateTimeToUnix
 };
